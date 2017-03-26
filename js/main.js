1e747521cb922ffced42492b0388323b4b4f5c88
Codija <pvlvcdavid@gmail.com>
2017-03-26 11:31:15 +0200

    Fixed map

--- js/main.js
+++ js/main.js
@@ -1,61 +1,55 @@
-var myIndex = 0;
-carousel();
+(function() {
+  var myIndex = 0;
+  carousel();
 
-function carousel() {
-    var i;
-    var x = document.getElementsByClassName("mySlides");
-    for (i = 0; i < x.length; i++) {
-       x[i].style.display = "none";
-    }
-    myIndex++;
-    if (myIndex > x.length) {myIndex = 1}
-    x[myIndex-1].style.display = "block";
-    setTimeout(carousel, 4000);
-}
-
-function myFunction() {
-    var x = document.getElementById("navbar");
-    if (x.className === "navbar") {
-        x.className += " responsive";
-    } else {
-        x.className = "navbar";
-    }
-}
-
-var modal = document.getElementById('scModal');
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+  function carousel() {
+      var i;
+      var x = document.getElementsByClassName("mySlides");
+      for (i = 0; i < x.length; i++) {
+         x[i].style.display = "none";
+      }
+      myIndex++;
+      if (myIndex > x.length) {myIndex = 1}
+      x[myIndex-1].style.display = "block";
+      setTimeout(carousel, 4000);
   }
-}
 
-var map;
-function initMap() {
-    map = new google.maps.Map(document.getElementById('map'), {
-      center: {lat: 44.843456, lng: 20.407123},
-      zoom: 16
-    });
-}
-
-$('a[href*="#"]:not([href="#"])').click(function() {
-  if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-    var target = $(this.hash);
-    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-    if (target.length) {
-      $('html, body').animate({
-        scrollTop: target.offset().top
-      }, 1000);
-      return false;
-    }
+  function myFunction() {
+      var x = document.getElementById("navbar");
+      if (x.className === "navbar") {
+          x.className += " responsive";
+      } else {
+          x.className = "navbar";
+      }
   }
-});
-$(window).scroll(function() {
-    if ($(this).scrollTop() > 100) {
-        $('.scroll-to-top').fadeIn();
-    } else {
-        $('.scroll-to-top').fadeOut();
-    }});
-  $('.scroll-to-top').on('click', function(e) {
-    e.preventDefault();
-    $('html, body').animate({scrollTop : 0}, 800);
-});
+
+  var modal = document.getElementById('scModal');
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  };
+
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+  $(window).scroll(function() {
+      if ($(this).scrollTop() > 100) {
+          $('.scroll-to-top').fadeIn();
+      } else {
+          $('.scroll-to-top').fadeOut();
+      }});
+    $('.scroll-to-top').on('click', function(e) {
+      e.preventDefault();
+      $('html, body').animate({scrollTop : 0}, 800);
+  });
+})();
